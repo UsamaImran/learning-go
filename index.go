a5a573594bb80c522e633627ccea1121eb328177
Usama Imran <uusamaimran@gmail.com>
2026-06-01 17:25:46 +0500

    Update index.go

--- index.go
+++ index.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 )
 
 func main (){
 	fmt.Print("before")
 	fmt.Println("Hello World")
-	fmt.Print("testing logger")
+	fmt.Println("testing logger")
+	
+	var value int 
+	fmt.Print(value)
+
+	common := "common variable"
+	common = "asdasdasd"
+
+	numberImplicit := 2300000032423423234
+	fmt.Println(common)
+	fmt.Println(numberImplicit)
+	fmt.Print(common,numberImplicit,"\n","asdasdasd", value, math.Pow(2,3), strconv.Atoi("12234"))
 }
